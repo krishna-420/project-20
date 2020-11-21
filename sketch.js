b0e68c1c427137ee73ef358e9900d9e15ff59e4e
krishna-420 <72797251+krishna-420@users.noreply.github.com>
2020-11-21 17:25:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 var car,wall;
-var speed , weight;
+var bullet, speed , weight;
 
 
 function setup() {
   createCanvas(1600,400);
   createSprite(400, 200, 50, 50);
-speed=ranodm(55,90)
-weight=random(400,1500)
+speed=ranodm(223,321)
+weight=random(30,52)
+thickness=random(22,83)
 car = createSprite(50,200,50,50);
 wall = createSprite(1500,200,60,height/2);
+wall2 = createSprite(1200,200,thickness,height/2);
 car.velocityX=speed;
 
 }
